b20c4d49d3d247a4d67364bf4f1ace7a03098851
Oli <62344268+OliverMunian@users.noreply.github.com>
2023-10-19 14:44:18 +0200

    Update index.js
    Basics

--- routes/index.js
+++ routes/index.js
@@ -1,48 +1,20 @@
 var express = require('express');
 var router = express.Router();
-const place = require('../models/place')
 
-router.post('/places', function(req, res) {
-    place.findOne({nickname: req.body.nickname, name: req.body.name, latitude: req.body.latitude, longitude:req.body.longitude})
-    .then(data =>{
-        if(data){
-            res.json('Profil existant !')
-        }
-        else{
-            const newPlace = new  place({
-                nickname: req.body.nickname,
-                name: req.body.name,
-                latitude: req.body.latitude,
-                longitude: req.body.longitude
-            })
-            newPlace.save().then(()=> res.json({result: true, location: newPlace}))
-        }
-    })
+const fetch = require('node-fetch');
+
+const NEWS_API_KEY = process.env.NEWS_API_KEY;
+
+router.get('/articles', (req, res) => {
+  fetch(`https://newsapi.org/v2/top-headlines?sources=the-verge&apiKey=${NEWS_API_KEY}`)
+    .then(response => response.json())
+    .then(data => {
+      if (data.status === 'ok') {
+        res.json({ articles: data.articles });
+      } else {
+        res.json({ articles: [] });
+      }
+    });
 });
-
-
-router.get('/places/:nickname', function(req,res){
-    place.findOne({nickname: req.params.nickname})
-    .then(data => {
-        if(data){
-            res.json({result: true, places:{data}})
-        }
-        else{
-            res.json({result: 'Ville non-trouvée !'})
-        }
-    })
-})
-
-router.delete('/places', function(req, res){
-    place.findOne({nickname: req.body.nickname, name: req.body.name})
-    .then(data => {
-        if(data){
-            data.deleteOne().then(()=> res.json)
-            res.json({result: true, places:{data}})
-        }
-    })
-})
-
-
 
 module.exports = router;
