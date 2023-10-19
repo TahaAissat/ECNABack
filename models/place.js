43fb15d4e035d205ddc2dad71ba884658ef5b285
TahaAissat <tahayacineaissat@gmail.com>
2023-10-19 14:47:20 +0200

    createEnv

--- models/place.js
+++ models/place.js
@@ -1,14 +1,6 @@
 var mongoose = require('mongoose')
 
 
-const placeSchema = mongoose.Schema({
-    nickname: String,
-    name: String,
-    latitude: Number,
-    longitude: Number
-})
 
 
-const place = mongoose.model('places', placeSchema)
 
-module.exports = place
